7a7915fcaafddd35ba0bff53cbb837e4796d4e43
Maxim Vdovenko <vdovenko.maxim@gmail.com>
2020-08-31 00:00:19 +0300

    add mobile style page - Get Started

--- src/js/index.js
+++ src/js/index.js
@@ -1205,7 +1205,16 @@ getStarted.sliderFn = function() {
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 5000
+    autoplaySpeed: 5000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          fade: false,
+          adaptiveHeight: true
+        }
+      }
+    ]
   })
 }
 
