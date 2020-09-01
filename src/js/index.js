94097c8450f2b9fe959f692a2903a0564b315d8a
Maxim Vdovenko <vdovenko.maxim@gmail.com>
2020-09-01 18:27:14 +0300

    fix fon in block - integrateTools

--- src/js/index.js
+++ src/js/index.js
@@ -525,21 +525,23 @@ const integrateTools = {
 integrateTools.init = function() {
   $(this.bl).addClass('active')
 
-  const controller = new ScrollMagic.Controller()
-  const scene = new ScrollMagic.Scene({
-    triggerElement: this.reviews, 
-    triggerHook: 'onEnter', 
-    offset: $(this.reviews).outerHeight()})
-    .setTween(this.reviews)
-    .addTo(controller)
-
-  scene.on('enter', () => {
-    $('body').addClass('addColor-blueLight')
-  })
-
-  scene.on('leave', () => {
-    $('body').removeClass('addColor-blueLight')
-  })
+  if ($(this.reviews).length) {
+    const controller = new ScrollMagic.Controller()
+    const scene = new ScrollMagic.Scene({
+      triggerElement: this.reviews, 
+      triggerHook: 'onEnter', 
+      offset: $(this.reviews).outerHeight()})
+      .setTween(this.reviews)
+      .addTo(controller)
+  
+    scene.on('enter', () => {
+      $('body').addClass('addColor-blueLight')
+    })
+  
+    scene.on('leave', () => {
+      $('body').removeClass('addColor-blueLight')
+    })
+  }
 
   animationAdd.act(this.reviews, $(this.reviews).outerHeight() / 2)
   animationAdd.act(this.instruments, $(this.instruments).outerHeight() / 2)
